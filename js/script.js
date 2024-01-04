dc9e19943302cb722ed384c5e73b11ca44e781fc
Tamanna Begum <145229830+TamannaBegum1@users.noreply.github.com>
2024-01-04 17:27:16 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// javaScript code here
+
+// jQuery code here
+$(function () {
+  // preloader start
+  $(window).load(function () {
+    $("#loading").fadeOut(500);
+  });
+  // preloader end
+});
